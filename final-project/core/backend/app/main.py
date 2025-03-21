4ead58e890764e31ff11640795b73b54f8050356
Ondřej Pohl <ondra.pohl@gmail.com>
2025-03-22 11:45:40 +1300

    #30: Updated API routing

--- final-project/core/backend/app/main.py
+++ final-project/core/backend/app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 
-app = FastAPI(title="DuoKani WaniLingo")
+app = FastAPI(
+    title="DuoKani WaniLingo",
+    docs_url="/docs",  # This will be /api/docs
+    openapi_url="/openapi.json",  # This will be /api/openapi.json
+    root_path="/api"  # This tells FastAPI all routes are under /api
+)
 
 @app.get("/health")
 async def health_check():
