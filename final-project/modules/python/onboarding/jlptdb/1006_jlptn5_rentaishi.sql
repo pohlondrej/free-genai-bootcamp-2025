c993dd434380a961c692f00e39d920bb48b85d8f
Ondřej Pohl <ondra.pohl@gmail.com>
2025-03-30 09:28:33 +1300

    #30: Added JLPT N5 import scripts

---
+++ final-project/modules/python/onboarding/jlptdb/1006_jlptn5_rentaishi.sql
@@ -0,0 +1,10 @@
+-- Insert JLPT N5 rentaishi into the words table
+INSERT INTO words (word_level, japanese, kana, romaji, english) VALUES
+('N5', 'あの', 'あの', 'ano', 'that'),
+('N5', '小さな', 'ちいさな', 'chiisana', 'small'),
+('N5', 'どんな', 'どんな', 'donna', 'what kind of'),
+('N5', 'どの', 'どの', 'dono', 'which'),
+('N5', 'こんな', 'こんな', 'konna', 'such'),
+('N5', 'この', 'この', 'kono', 'this'),
+('N5', '大きな', 'おおきな', 'ookina', 'big'),
+('N5', 'その', 'その', 'sono', 'that');
